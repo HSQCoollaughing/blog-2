0d7d42a148ec516742433d29d76230fca1dd8151
ZhangBing <1484054651@qq.com>
2016-12-06 20:09:44 +0800

    d

--- gulpfile.js
+++ gulpfile.js
@@ -5,28 +5,47 @@ var gulp = require('gulp'),
     imagemin = require('gulp-imagemin');
 //JS压缩
 gulp.task('jsuglify', function() {
-    return gulp.src('././public/js/**/.js')
+    return gulp.src('public/js/src/*.js')
         .pipe(uglify())
-        .pipe(gulp.dest('././public/js/'));
+        .pipe(gulp.dest('public/js/src/'))
 });
+gulp.task('jsuglify2', function() {
+    return gulp.src('public/js/src/schemes/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('public/js/src/schemes/'))
+});
+gulp.task('jsuglify3', function() {
+    return gulp.src('public/lib/jquery_layload/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('public/lib/jquery_layload'))
+});
+
 //public-fancybox-css压缩
 gulp.task('fancybox:css', function() {
-    return gulp.src('././public/fancybox/jquery.fancybox.css')
+    return gulp.src('public/lib/fancybox/jquery.fancybox.css')
         .pipe(cssmin())
-        .pipe(gulp.dest('././public/fancybox/'));
+        .pipe(gulp.dest('public/lib/fancybox/'));
 });
 //CSS压缩
 gulp.task('cssmin', function() {
-    return gulp.src('././public/css/style.css')
+    return gulp.src('public/css/*.css')
         .pipe(cssmin())
-        .pipe(gulp.dest('././public/css/'));
+        .pipe(gulp.dest('public/css/'));
 });
 //图片压缩
 gulp.task('images', function() {
-    gulp.src('././public/img/*.*')
+    gulp.src(['public/images/*.*'])
         .pipe(imagemin({
             progressive: false
         }))
-        .pipe(gulp.dest('././public/img/'));
+        .pipe(gulp.dest('public/images/'));
 });
-gulp.task('build', ['jsuglify', 'cssmin', 'images',  'fancybox:css']);
+gulp.task('img', function() {
+    gulp.src(['public/img/*.*'])
+        .pipe(imagemin({
+            progressive: false
+        }))
+        .pipe(gulp.dest('public/img/'));
+});
+//前端构建优化  全部压缩  暂时不做合并文件的请求
+gulp.task('build', ['jsuglify','jsuglify2','jsuglify3', 'cssmin', 'images',"img",  'fancybox:css']);
